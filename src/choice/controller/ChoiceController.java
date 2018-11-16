178aeb23c5d2a0ff987084882f556e257234fcc3
Bowen <tbow1382@740s-pl-120.local>
2018-11-16 09:11:08 -0700

    Many things were done here:
    ChoiceController: imported many important things (like JOptionPane, ImageIcon, CharacterModel).
    Also created arrays for ImageIcon and CharacterModel.
    Created arrayInitialization().
    
    CharacterModel:
    Didn't really do ANYTHING in here.

--- src/choice/controller/ChoiceController.java
+++ src/choice/controller/ChoiceController.java
@@ -1,15 +1,49 @@
 package choice.controller;
 
+import javax.swing.JOptionPane;
+
+import choice.model.CharacterModel;
+
+import javax.swing.ImageIcon;
+
 public class ChoiceController
 {
+	private ImageIcon[] icons;
+	
+	private CharacterModel[] characterArray;
+	
 	public ChoiceController()
 	{
 		
+		icons = new ImageIcon[6];
+		
+		icons[0] = new ImageIcon(getClass().getResource(""));
+		
+		
+		characterArray = new CharacterModel[10];
 	}
 	
 	public void start()
 	{
-		
+		arrayInitialization();
 	}
+	
+	private void arrayInitialization()
+	{
+		JOptionPane.showMessageDialog(null, "When you initialize an array of objects, \nit is filled with a bunch of null");
+		JOptionPane.showMessageDialog(null, "If you try to use them it will crash (");
+		
+		JOptionPane.showMessageDialog(null, "But f you fill the array with values it is good ");
+		
+		for (int index = 0; index < characterArray.length; index += 1)
+		{
+			if (index % 2 == 0)
+			{
+				characterArray[index] = new CharacterModel();
+			}
+			
+		}
+	}
+	
 	
 }
